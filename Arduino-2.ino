367eae6e75da336ebef49fc520cf8eb058649b3d
Luis Antonio <luisjimenezvargas2006@gmail.com>
2025-04-30 10:05:11 +0200

    Codigo video 4

--- Arduino-2.ino
+++ Arduino-2.ino
@@ -1,1 +1,39 @@
-Hola mundo hoy vamos a hacer mucho intentos de comandos.
+// video capitulo 4
+//Primer codigo
+int LED = 3;
+int Brillo;
+
+void setup()
+{
+    pinMode(LED,OUTPUT);
+}
+
+void loop()
+{
+    for(Brillo = 0;Brillo < 256; Brillo++)  // Brillo de el led ira aumetando hasta llegar al maximo.
+    {
+        analogWrite(LED,Brillo);
+        delay(15;)
+    }
+
+    for(Brillo = 255;Brillo >= 0; Brillo--)  //Cunado el brillo este al maximo este ira disminuyendo hasta apagarse.
+    {
+        analogWrite(LED,Brillo);
+        delay(15;)
+    }
+}
+//Segundo codigo
+int LED = 3;
+int Brillo;
+int POT = 0;
+
+void setup()
+{
+    pinMode(LED,OUTPUT);  //las entradas analogicas no requieren de inicializacion.
+}
+
+void loop()
+{
+    Brillo = analogRead(POT) / 4;
+    analogWrite(LED,Brillo);
+}
